f7a5a6249ed34373f6c55e4b7f9ae898b91927e5
Your Name <you@example.com>
2019-03-26 09:32:49 +0700

    lol2

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import './App.css';
 import Product from "./Product";
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      products :[
+        {
+          judul : 'Powerbank 1',
+          harga : '100000'
+        },
+        {
+          judul : 'Powerbank 2',
+          harga : '200000'
+        },
+        {
+          judul : 'Kabel data  USB3',
+          harga : '30000'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="row">
         <header className="App-header">
-          <Product className="column" judul="Power Bank" harga="10.0000"/>
-          <Product className="column" judul="Power Bank" harga="10.0000"/>
-          <Product className="column" judul="Power Bank" harga="10.0000"/>
-          <Product className="column" judul="Power Bank" harga="10.0000"/>
+          {
+            this.state.products.map((data,key)=>
+            <Product className="column" judul={data.judul} harga={data.harga}/>
+            )
+          }
         </header>
       </div>
     );
